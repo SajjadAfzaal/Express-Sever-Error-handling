c5ebd470b9882086e2037cbd55a39465665bd0c4
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-02-06 15:55:36 +0500

    initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res, next){
+    res.send("Hello World!");
+});
+
+// Server checking
+app.listen(3000, function(){
+    console.log("Server is listening on port 3000");
+})
