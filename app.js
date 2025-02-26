95a46718c3502a9f6daf755ddf84001767b29e67
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-02-26 20:14:52 +0500

    Mongo Configuration

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongooseconnection = require("./config/mongoose");
 
-app.get('/', function(req, res, next){
-    try{
-        res.send("Hello World!");
-    }
-    catch(err){
-        next(err);
-    }
-    
+let data = [1, 2, 3, 4, 5];
+
+app.get("/", function (req, res, next) {
+  try {
+    res.send("Hello World!");
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/enter/:number", (req, res) => {
+  data.push(parseInt(req.params.number));
+  res.send(data);
 });
 
 //Error handler
-app.use((err, req, res, next,)=>{
-    res.status(500).send(err.message);
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
 });
 
 // Server checking
-app.listen(3000, function(){
-    console.log("Server is listening on port 3000");
-})
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000");
+});
